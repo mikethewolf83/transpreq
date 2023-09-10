866a0d58756f9281f51eedc512fbfa24edb41542
Maikel Nieves Matamoros <mikethewolf83@gmail.com>
2023-09-09 22:54:10 -0400

    Improved firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDcUJf89sZdDh3UaFixq_yGhJ_YtBDLoq8",
-  authDomain: "transpreq-data.firebaseapp.com",
-  projectId: "transpreq-data",
-  storageBucket: "transpreq-data.appspot.com",
-  messagingSenderId: "163722423964",
-  appId: "1:163722423964:web:8b55409607a2b158834759"
-};
+  apiKey: 'AIzaSyDcUJf89sZdDh3UaFixq_yGhJ_YtBDLoq8',
+  authDomain: 'transpreq-data.firebaseapp.com',
+  projectId: 'transpreq-data',
+  storageBucket: 'transpreq-data.appspot.com',
+  messagingSenderId: '163722423964',
+  appId: '1:163722423964:web:8b55409607a2b158834759'
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}
