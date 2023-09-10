9ae13a259b6d9474fae46d9b427cc0b82444c630
Maikel Nieves Matamoros <mikethewolf83@gmail.com>
2023-09-09 22:18:09 -0400

    Added firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcUJf89sZdDh3UaFixq_yGhJ_YtBDLoq8",
+  authDomain: "transpreq-data.firebaseapp.com",
+  projectId: "transpreq-data",
+  storageBucket: "transpreq-data.appspot.com",
+  messagingSenderId: "163722423964",
+  appId: "1:163722423964:web:8b55409607a2b158834759"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
